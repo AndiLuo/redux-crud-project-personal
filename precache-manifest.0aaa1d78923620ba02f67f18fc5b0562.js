f12cb2ee82eaa89a5dc9db7196002e6f70567c36
unknown <aluo5731@gmail.com>
2020-05-21 18:21:02 -0700

    Updates

---
+++ precache-manifest.0aaa1d78923620ba02f67f18fc5b0562.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91caf14f193ab7a02b7b31fa7cd141dd",
+    "url": "/pinstagram-redux-local/index.html"
+  },
+  {
+    "revision": "b5064ef29ff1bdabfc82",
+    "url": "/pinstagram-redux-local/static/css/2.ecc39376.chunk.css"
+  },
+  {
+    "revision": "1c1bbc822b590b8e31ed",
+    "url": "/pinstagram-redux-local/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b5064ef29ff1bdabfc82",
+    "url": "/pinstagram-redux-local/static/js/2.f6cdab52.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/pinstagram-redux-local/static/js/2.f6cdab52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1bbc822b590b8e31ed",
+    "url": "/pinstagram-redux-local/static/js/main.b1b56da9.chunk.js"
+  },
+  {
+    "revision": "5b77b27647facce22fd9",
+    "url": "/pinstagram-redux-local/static/js/runtime-main.804127f8.js"
+  },
+  {
+    "revision": "dc252b3ba7baaca7c8efd13ac5fc4bf4",
+    "url": "/pinstagram-redux-local/static/media/spinner_ex.dc252b3b.gif"
+  }
+]);
